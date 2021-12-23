8789b3440c5e9847f63da15b866498155cbe011d
Nicolas Buttarelli <nbuttarelli@gmail.com>
2021-12-23 16:17:51 -0300

    deploy fixes

--- src/flows/Home.js
+++ src/flows/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Home() {
     return (
         <div className='home'>
-            <h1>Home</h1>
+            <h2>React Flow Research</h2>
+            <p>Access the diferent examples through the menu. </p>
+            <p>Last example is the interesting one as gives you the tools to create, edit and explore react flow functionalities.</p>
         </div>
     );
 }
